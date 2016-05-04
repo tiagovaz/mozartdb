aecf66cc4d6899a8726352a4f42fbc4dac442485
Tiago Bortoletto Vaz <tiago@debian.org>
2016-05-04 13:21:35 -0400

    Labels representation in Fr.

--- mozartweb/models.py
+++ mozartweb/models.py
@@ -5,7 +5,7 @@ from django.utils.encoding import python_2_unicode_compatible
 
 @python_2_unicode_compatible
 class City(models.Model):
-    name = models.CharField(max_length=150)
+    name = models.CharField("Nom de la ville", max_length=150)
 
     def __str__(self):
         return self.name
@@ -16,7 +16,7 @@ class City(models.Model):
 
 @python_2_unicode_compatible
 class Country(models.Model):
-    name = models.CharField(max_length=150)
+    name = models.CharField("Nom du pays", max_length=150)
 
     def __str__(self):
         return self.name
@@ -27,7 +27,7 @@ class Country(models.Model):
 
 @python_2_unicode_compatible
 class Place(models.Model):
-    venue = models.CharField(max_length=200)
+    venue = models.CharField("Lieu", max_length=200)
     city = models.ForeignKey('City')
     country = models.ForeignKey('Country')
 
@@ -40,7 +40,7 @@ class Place(models.Model):
 
 @python_2_unicode_compatible
 class Type(models.Model):
-    type = models.CharField(max_length=200)
+    type = models.CharField("Nature de l'évènement", max_length=200)
 
     def __str__(self):
         return self.type
@@ -51,7 +51,7 @@ class Type(models.Model):
 
 @python_2_unicode_compatible
 class Performer(models.Model):
-    name = models.CharField(max_length=200)
+    name = models.CharField("Nom de l'interprète", max_length=200)
 
     def __str__(self):
         return self.name
@@ -62,18 +62,18 @@ class Performer(models.Model):
 
 @python_2_unicode_compatible
 class Speaker(models.Model):
-    name = models.CharField(max_length=200)
+    name = models.CharField("Nome du/de la conférencier/ère", max_length=200)
 
     def __str__(self):
         return self.name
 
     class Meta:
-        verbose_name = "Conférencier"
-        verbose_name_plural = "Conférencier"
+        verbose_name = "Conférencier/ère"
+        verbose_name_plural = "Conférencier/ère"
 
 @python_2_unicode_compatible
 class Piece(models.Model):
-    description = models.CharField(max_length=200)
+    description = models.CharField("Titre de l'œuvre interpretée", max_length=200)
 
     def __str__(self):
         return self.description
@@ -85,13 +85,13 @@ class Piece(models.Model):
 @python_2_unicode_compatible
 class Event(models.Model):
     """The main class for all 'Mozart' events."""
-    title = models.CharField(max_length=200, null=True, blank=True)
-    reference = models.CharField(max_length=200, null=True, blank=True)
-    place = models.ForeignKey('Place')
-    type = models.ForeignKey('Type')
-    performer = models.ManyToManyField('Performer')
-    speaker = models.ManyToManyField('Speaker')
-    piece = models.ManyToManyField('Piece')
+    title = models.CharField("Titre ou description de l'évènement", max_length=200, null=True, blank=True)
+    reference = models.CharField("Référence", max_length=200, null=True, blank=True)
+    place = models.ForeignKey('Place', verbose_name='Lieu')
+    type = models.ForeignKey('Type', verbose_name="Nature de l'évènement")
+    performer = models.ManyToManyField('Performer', verbose_name="Interprètes")
+    speaker = models.ManyToManyField('Speaker', verbose_name="Conférenciers/ères")
+    piece = models.ManyToManyField('Piece', verbose_name="Œuvres interpretées")
     date = models.DateField()
 
     class Meta:
