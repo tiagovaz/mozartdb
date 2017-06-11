2e319cc785d6ec8e26e98a0e3f016dd6651ee4c2
Tiago Bortoletto Vaz <tiago@acaia.ca>
2017-06-11 13:23:36 +0000

    Custom datatables.

--- mozartweb/static/sbadmin2/test.js
+++ mozartweb/static/sbadmin2/test.js
@@ -1,5 +1,21 @@
     $(document).ready(function() {
         $('#dataTables-example').DataTable({
-                responsive: true
+               "language": {
+                   "lengthMenu": "Afficher _MENU_",
+                   "zeroRecords": "Aucun résultat",
+                   "info": "Page _PAGE_ de _PAGES_",
+                   "search":         "Rechercher :",
+                   "infoEmpty": "No records available",
+                   "infoFiltered": "(filtered from _MAX_ total records)",
+                   "paginate": {
+                       "first":      "Première",
+                       "last":       "Dernière",
+                       "next":       "Suivante",
+                       "previous":   "Précedente"
+                   }
+               },
+               "lengthMenu": [[25, 50, 100, -1], [25, 50, 100, "Tous"]],
+                responsive: true,
+                stateSave: true
         });
     });
