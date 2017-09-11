4053981ce1743f450d5f29a9669fce87c4463608
Tiago Bortoletto Vaz <tiago@acaia.ca>
2017-09-11 12:48:06 -0400

    Fix venue filter.

--- mozartweb/filters.py
+++ mozartweb/filters.py
@@ -12,9 +12,9 @@ class EventFilter(django_filters.FilterSet):
     piece__name = django_filters.CharFilter(label='Œuvre', lookup_expr='icontains')
     reference__article_title = django_filters.CharFilter(label="Titre de l'article", lookup_expr='icontains')
     reference__journal_title = django_filters.CharFilter(label="Titre du journal", lookup_expr='icontains')
-    place__city = django_filters.ModelChoiceFilter(label="Ville", queryset=City.objects.all())
-    place__country = django_filters.ModelChoiceFilter(label="Pays", queryset=Country.objects.all())
-    place__venue = django_filters.CharFilter(label="Lieu", lookup_expr='icontains')
+    places__city = django_filters.ModelChoiceFilter(label="Ville", queryset=City.objects.all())
+    places__country = django_filters.ModelChoiceFilter(label="Pays", queryset=Country.objects.all())
+    places__venue = django_filters.CharFilter(label="Lieu", lookup_expr='icontains')
     speech__title = django_filters.CharFilter(label="Titre de la conférence", lookup_expr='icontains')
     speech__speaker__last_name = django_filters.CharFilter(label="Conférencier-ère", lookup_expr='icontains')
     #year = django_filters.MethodFilter(label='Année', action='year_range')
@@ -41,9 +41,9 @@ class EventFilter(django_filters.FilterSet):
             'speech__speaker__last_name',
             'reference__article_title',
             'reference__journal_title',
-            'place__venue',
-            'place__city',
-            'place__country',
+            'places__venue',
+            'places__city',
+            'places__country',
             'start_date',
             'pdf_checked',
             'comment__content',
