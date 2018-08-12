bdfdf81f350d75ddd0c398b14c45efae756912df
Tiago Bortoletto Vaz <tiago@acaia.ca>
2018-08-12 07:55:51 -0400

    Some code cleanup

--- mozartweb/filters.py
+++ mozartweb/filters.py
@@ -20,23 +20,16 @@ class EventFilter(django_filters.FilterSet):
     places__venue = django_filters.CharFilter(label="Lieu", method='custom_venue_filter')
     speech__title = django_filters.CharFilter(label="Titre de la conférence", method='custom_speech_filter')
     speech__speaker = django_filters.CharFilter(label="Conférencier-ère", method='custom_speaker_filter')
-    #year = django_filters.MethodFilter(label='Année', action='year_range')
     start_date = django_filters.DateFromToRangeFilter(label="Date de l'événement (début - fin jj/mm/aaaa)")
-    #year = django_filters.NumberFilter(label='Année', name='start_date', lookup_expr='year')
-    #month = django_filters.NumberFilter(label='Mois', name='start_date', lookup_expr='month')
     year_insertion = django_filters.NumberFilter(label="Année d'insertion", name='created_on', lookup_expr='year')
     created_on = django_filters.DateFromToRangeFilter(label="Date d'insertion (début - fin jj/mm/aaaa)")
     comment__content = django_filters.CharFilter(label="Commentaires", method='custom_comments_filter')
     info__content = django_filters.CharFilter(label="Informations complémentaires", method='custom_info_filter')
     created_by__username = django_filters.ModelChoiceFilter(label="Utilisateur", queryset=User.objects.all())
 
- #   def year_range(self, queryset, value):
- #       return queryset.filter()
-
-
 # With the new version of django-filter we can remove the duplication of
 # broadcasting model, since it's now possible to look for many models in a
-# query:
+# query
 
     wordDict = {'ss':'ß', 'oe':'ö', 'ue':'ü', 'ae':'ä'}
     iwordDict = {'ß':'ss', 'ö':'oe', 'ü':'ue', 'ä':'ae'}
@@ -140,10 +133,3 @@ class EventFilter(django_filters.FilterSet):
             'year_insertion',
             'created_on',
         ]
-
-#
-#    def filter_title(self, queryset, value):
-#        return queryset.filter(relates_to_broadcasting__title__icontains=value) | queryset.filter(title__icontains=value)
-#
-#    def filter_reference__author(self, queryset, value):
-#        return queryset.filter(reference__author__last_name__icontains=value) | queryset.filter(reference__author__first_name__icontains=value)
